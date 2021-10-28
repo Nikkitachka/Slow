620767bdfd17771adad1614deb24af9aa2ca9188
Peter <plarochkin@icloud.com>
2021-10-28 22:18:41 +0300

    First Add

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slow
+//
+//  Created by Петр Ларочкин on 28.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
