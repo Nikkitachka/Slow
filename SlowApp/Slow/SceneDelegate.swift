16425a212628f5227e6b5d34c6e24b64f426bfc3
Игорь Багдасарян <clauzdev@MacBook-Pro-Igor.local>
2021-11-09 00:02:47 +0300

    Start

--- SlowApp/Slow/SceneDelegate.swift
+++ SlowApp/Slow/SceneDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  SceneDelegate.swift
-//  Slow
+//  loginScreen
 //
-//  Created by Петр Ларочкин on 28.10.2021.
+//  Created by Игорь Багдасарян on 02.11.2021.
 //
 
 import UIKit
@@ -13,12 +13,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-      
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController() // Your initial view controller.
-        window.makeKeyAndVisible()
-        self.window = window
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = UINavigationController(rootViewController: SignUpController())
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -47,9 +50,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-
-        // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
---
+++ SlowApp/Slow/SignUpController.swift
@@ -0,0 +1,153 @@
+//
+//  SignUpController.swift
+//  loginScreen
+//
+//  Created by Игорь Багдасарян on 03.11.2021.
+//
+
+import UIKit
+
+class SignUpController: UIViewController {
+    
+    @objc
+    func goToLoginController() {
+        navigationController?.pushViewController(LoginController(), animated: true)
+    }
+    
+    let plusPhotoButton: UIButton = {
+        let button = UIButton(type: .custom)
+        let imageLogo = UIImage(named: "tg_image_592672127.jpeg")
+        
+        button.setImage(imageLogo, for: .normal)
+        button.layer.cornerRadius = (imageLogo?.size.width ?? 175) / 2
+        button.imageView?.contentMode = .scaleAspectFill
+        button.imageView?.layer.cornerRadius = (imageLogo?.size.width ?? 175) / 2
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.layer.cornerRadius = 12
+        tf.layer.borderWidth = 1
+        tf.layer.borderColor = UIColor(white: 0, alpha: 1).cgColor
+        tf.font = UIFont.systemFont(ofSize: 24)
+        
+        let centeredParagraphStyle = NSMutableParagraphStyle()
+        centeredParagraphStyle.alignment = .center
+        
+        tf.attributedPlaceholder = NSAttributedString(
+            string: "почта@почта.ру",
+            attributes: [.paragraphStyle: centeredParagraphStyle]
+        )
+        
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.layer.cornerRadius = 12
+        tf.layer.borderWidth = 1
+        tf.layer.borderColor = UIColor(white: 0, alpha: 1).cgColor
+        tf.font = UIFont.systemFont(ofSize: 24)
+        
+        let centeredParagraphStyle = NSMutableParagraphStyle()
+        centeredParagraphStyle.alignment = .center
+        
+        tf.attributedPlaceholder = NSAttributedString(
+            string: "пароль",
+            attributes: [.paragraphStyle: centeredParagraphStyle]
+        )
+        
+        return tf
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Зарегистрироваться", for: .normal)
+        button.backgroundColor = UIColor(red: 168/255, green: 242/255, blue: 245/255, alpha: 1)
+        button.layer.cornerRadius = 12
+        if let titleLabel = button.titleLabel {titleLabel.font = UIFont.systemFont(ofSize: 24)}
+        button.setTitleColor(UIColor(white: 0, alpha: 1), for: .normal)
+        button.layer.borderColor = UIColor(white: 0, alpha: 1).cgColor
+        button.layer.borderWidth = 1
+        return button
+    }()
+    
+    let accExistsButton: UIButton = {
+        let button = UIButton(type: .system)
+        let yourAttributes: [NSAttributedString.Key: Any] = [
+              .font: UIFont.systemFont(ofSize: 24),
+              .foregroundColor: UIColor.blue,
+              .underlineStyle: NSUnderlineStyle.single.rawValue
+        ]
+        let attributeString = NSMutableAttributedString(
+                string: "Есть аккаунт",
+                attributes: yourAttributes
+        )
+        button.setAttributedTitle(attributeString, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(goToLoginController), for: .touchUpInside)
+        return button
+    }()
+    
+    let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo.png")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 244/255, green: 244/255, blue: 244/255, alpha: 1)
+        view.addSubview(plusPhotoButton)
+        NSLayoutConstraint.activate([
+            plusPhotoButton.heightAnchor.constraint(equalToConstant: 175),
+            plusPhotoButton.widthAnchor.constraint(equalToConstant: 175),
+            plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 90)
+        ])
+        
+        view.addSubview(logoImageView)
+        
+        NSLayoutConstraint.activate([
+            logoImageView.heightAnchor.constraint(equalToConstant: 56),
+            logoImageView.widthAnchor.constraint(equalToConstant: 100),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -120)
+        ])
+        
+        setupInputFields()
+//        view.addSubview(accExistsButton)
+//
+//        NSLayoutConstraint.activate([
+//            accExistsButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            accExistsButton.bottomAnchor.constraint(equalTo: .bottomAnchor, constant: -200)
+//        ])
+//
+    }
+    
+    
+    fileprivate func setupInputFields() {
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton, accExistsButton])
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 15
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 60),
+            stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
+            stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40),
+            stackView.heightAnchor.constraint(equalToConstant: 265)
+            ])
+
+    }
+}
+
